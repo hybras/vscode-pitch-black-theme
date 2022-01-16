a9cfbaa3997d05817667bb71e00d14f32deb5c00
Hybras <24651269+hybras@users.noreply.github.com>
2022-01-15 20:59:45 -0800

    Delete sync-built-in-theme.sh

--- themes/sync-built-in-theme.sh
+++
@@ -1,2 +0,0 @@
-curl -O https://raw.githubusercontent.com/microsoft/vscode/main/extensions/theme-defaults/themes/dark_vs.json
-curl -O https://raw.githubusercontent.com/microsoft/vscode/main/extensions/theme-defaults/themes/dark_plus.json
